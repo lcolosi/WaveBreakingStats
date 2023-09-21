7ba360a6b63794d32e2f51be5e11110d4d4283fd
Luke Colosi <lcolosi@ucsd.edu>
2023-09-21 16:43:41 +0000

    Added shift, Nstd, and tcheck to plug and play parameters.

--- src/Main.m
+++ src/Main.m
@@ -13,14 +13,17 @@ clc, clearvars, close all
 
 % Directories
 dirRaw = 'X:\\TFO_2021\Processed\VIDEO\16bit_TIF_Frames\20210519\';         % Raw (non-georeferenced) video images for a given flight
-dirout = 'X:\\TFO_2021\Processed\VIDEO\Trimble\20210519\';                  % Trimble processed (georeferenced) video images for a given flight
+dirProc = 'X:\\TFO_2021\Processed\VIDEO\Trimble\20210519\';                 % Trimble processed (georeferenced) video images for a given flight
 dirV = 'D:\DEPLOYMENTS\TFO_2021\figs\video\Quality_Control\';               % Verification figures
 
 % Flight stability criteria parameters
-maxPer=[25 25];                                                             % Maximum percent of flight track to be removed at the begin and end of the track. For example, maxPer = [25,25] means that at a minimium, 50 percent (from 25% - 75%) of the flight track will be used in analysis with the 0-25% and 75%-100% removed from the flight track.  
-sigRoll=3;                                                                  % Maximum allowed roll standard deviations for a stable segment.    
-sigPitch=3;                                                                 % Maximum allowed pitch standard deviations for a stable segment.
-sigHeading=6;                                                               % Maximum allowed heading standard deviations for a stable segment.
+maxPer = [25 25];                                                             % Maximum percent of flight track to be removed at the begin and end of the track. For example, maxPer = [25,25] means that at a minimium, 50 percent (from 25% - 75%) of the flight track will be used in analysis with the 0-25% and 75%-100% removed from the flight track.  
+sigRoll = 3;                                                                  % Maximum allowed roll standard deviations for a stable segment.    
+sigPitch = 3;                                                                 % Maximum allowed pitch standard deviations for a stable segment.
+sigHeading = 6;                                                               % Maximum allowed heading standard deviations for a stable segment.
+Shift = [-0.8*maxPer(1) 0.8*maxPer(2);0.8*maxPer(1) -0.8*maxPer(2);0 0];      % Shifts in flight track for finding a stable fligt period
+Nstd = 4;                                                                   % Number of standard deviations of either roll, pitch, or heading that constitute an abrupt change in attitude of the plane
+tCheck = 7;                                                                 % Time interval between the jth roll/pitch/heading observation to check if their is an abrupt change in attitude shortly after the jth observation.
 
 % Select process to run (0 or 1)
 option_ploton = 1;                                                          % Plot verification figures
@@ -30,25 +33,25 @@ option_ploton = 1;                                                          % Pl
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Rename EO directory in dirout path as Trimble_EO 
-if isfolder([dirout 'EO'])
-    movefile([dirout 'EO'],[dirout 'Trimble_EO']);
+if isfolder([dirProc 'EO'])
+    movefile([dirProc 'EO'],[dirProc 'Trimble_EO']);
 end
 
 % Obtain filenames of all EO files 
-D=dir([dirout 'Trimble_EO\' '*EO.txt']);
+D = dir([dirProc 'Trimble_EO\' '*EO.txt']);
 
 % Set path to EO file and its directory  
-EOpath=[dirout 'Trimble_EO\' D(1).name];
-EOdir=[dirout 'Trimble_EO\'];
+EOpath = [dirProc 'Trimble_EO\' D(1).name];
+EOdir = [dirProc 'Trimble_EO\'];
 
 % Obtain number of lines in the EO text file header 
-Counter=HeaderPosition(EOpath,'(sec)');
+Counter = HeaderPosition(EOpath,'(sec)');
 
 % Load trajectory and attitude data 
 A = importdata(EOpath,' ',Counter);
 
 % Get the text component of data (timestamp into column 1, track number and image number into column 2)
-An=cropText(A);
+An = cropText(A);
 
 % Determine the start and end time indices for each flight tracks.
 tracks = DefineTracks(An);
@@ -61,18 +64,18 @@ tracks = DefineTracks(An);
 % whitecap coverage will not be computed.  
 
 % Identify track stability based on input criteria
-trackTag = trackSteady(A,tracks,maxPer,sigRoll,sigPitch,sigHeading,An);
+trackTag = trackSteady(A,tracks,maxPer,sigRoll,sigPitch,sigHeading,Shift,Nstd,tCheck,An);
 
-% Plot out headings in flight tracks, with all other data (used portion of 
+% Plot headings in flight tracks, with all other data (used portion of 
 % the track, whether is it labeled as steady or not).
-if ploton == true
+if option_ploton == 1
 
     % Create a subdirectory for flight trajectory and stability plots
-    dirstab=[dirV 'Flight_Trajectories_And_Stability\'];
-    if ~exist(dirstab, 'dir'), mkdir(dirstab); end
+    dirTS = [dirV 'Flight_Trajectories_And_Stability\'];
+    if ~exist(dirTS, 'dir'), mkdir(dirTS); end
     
     % Generate plots
-    plotOutTracks(A,tracks,trackTag,dirstab,An,dirout);
+    plotOutTracks(A,tracks,trackTag,dirTS,An,dirProc);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -95,9 +98,9 @@ return
 % meanOriginal is the mean lighting in every image
 
 winSize=7;
-[meanIm,stdIm,RM_Nr,meanOriginal]=getImMeanStd_V5(dirRaw,D_Im,tracks_Im,trackTag,tracks,winSize,dirout);
+[meanIm,stdIm,RM_Nr,meanOriginal]=getImMeanStd_V5(dirRaw,D_Im,tracks_Im,trackTag,tracks,winSize,dirProc);
 
-dirV=[dirout 'Saved_Matlab_Data\'];
+dirV=[dirProc 'Saved_Matlab_Data\'];
 if ~exist(dirV, 'dir'), mkdir(dirV); end
 save([dirV 'mean_Images_Per_Track.mat'],'meanIm','stdIm','RM_Nr','meanOriginal');
 
@@ -108,12 +111,12 @@ save([dirV 'mean_Images_Per_Track.mat'],'meanIm','stdIm','RM_Nr','meanOriginal')
 
 % Export images with vignetting removed
 % dirProcessed='\\Airseaserver28\d\MASS\RAW\LCDRI_2017\VIDEO\TIFF_16BIT_EXPORT_Corrected\20170323\';
-dirProcessed=[dirout 'nongeoreferenced_Images_With_Vignetting_Removed_V2\'];
+dirProcessed=[dirProc 'nongeoreferenced_Images_With_Vignetting_Removed_V2\'];
 if ~exist(dirProcessed, 'dir'), mkdir(dirProcessed); end
 mina2=saveImage_test(dirRaw,D_Im,tracks_Im,trackTag,tracks,meanIm,stdIm,dirProcessed);
 
 %Save mask images
-dirProcessed=[dirout 'mask_For_Determining_High_Glint_Areas_V2\'];
+dirProcessed=[dirProc 'mask_For_Determining_High_Glint_Areas_V2\'];
 if ~exist(dirProcessed, 'dir'), mkdir(dirProcessed); end
 mina2=saveImageMask(dirRaw,D_Im,tracks_Im,trackTag,tracks,meanIm,stdIm,dirProcessed);
 
@@ -122,7 +125,7 @@ mina2=saveImageMask(dirRaw,D_Im,tracks_Im,trackTag,tracks,meanIm,stdIm,dirProces
 stdMag=-0.1;
 [Glint,Glint_mask]=det_glint(meanIm,stdMag,tracks,trackTag);
 
-dirV=[dirout 'Quality_Control\Vignetting\'];
+dirV=[dirProc 'Quality_Control\Vignetting\'];
 if ~exist(dirV, 'dir'), mkdir(dirV); end
 % Finally, the original and processed images are plotted out for
 % comparison. The mean image and mean std are also plotted out. 
@@ -147,8 +150,8 @@ for i=11:12%1:length(tracks)
         yy=nanmean(A.data(tracks(i).Indices(1):tracks(i).Indices(2),2));
         [proj_lat,proj_lon]=utm2deg(xx,yy,utmZone);
         
-        dirin_img = [dirout 'nongeoreferenced_Images_With_Vignetting_Removed_V2\Track_' num2str(i) '\']; %directory of all tif imagery to use for this single flight
-        dirin_prj = dirout;
+        dirin_img = [dirProc 'nongeoreferenced_Images_With_Vignetting_Removed_V2\Track_' num2str(i) '\']; %directory of all tif imagery to use for this single flight
+        dirin_prj = dirProc;
         
         buildBat(dirin_img,dirin_prj,proj_lat,proj_lon,i,camName);
     end
@@ -161,8 +164,8 @@ for i=11:12%1:length(tracks)
         yy=nanmean(A.data(tracks(i).Indices(1):tracks(i).Indices(2),2));
         [proj_lat,proj_lon]=utm2deg(xx,yy,utmZone);
         
-        dirin_img = [dirout 'mask_For_Determining_High_Glint_Areas\Track_' num2str(i) '\']; %directory of all tif imagery to use for this single flight
-        dirin_prj = dirout;
+        dirin_img = [dirProc 'mask_For_Determining_High_Glint_Areas\Track_' num2str(i) '\']; %directory of all tif imagery to use for this single flight
+        dirin_prj = dirProc;
         
         buildBatMask(dirin_img,dirin_prj,proj_lat,proj_lon,i,camName);
     end
@@ -171,8 +174,8 @@ end
 % Execute .bat files
 for i=11:12%1:length(tracks)
     if trackTag(i).stable==1
-        fileNN=dir([dirout 'Project\Track_' num2str(i) '\' '*.bat']);
-        system([dirout 'Project\Track_' num2str(i) '\' fileNN.name]);
+        fileNN=dir([dirProc 'Project\Track_' num2str(i) '\' '*.bat']);
+        system([dirProc 'Project\Track_' num2str(i) '\' fileNN.name]);
 %         while ~exist([dirout 'Output\Track_' num2str(i) '\blank.txt'])
 %            pause(300);
 %            fprintf('ne')
@@ -183,8 +186,8 @@ end
 
 for i=11:12%1:length(tracks)
     if trackTag(i).stable==1
-        fileNN=dir([dirout 'Project\TrackMask_' num2str(i) '\' '*.bat']);
-        system([dirout 'Project\TrackMask_' num2str(i) '\' fileNN.name]);
+        fileNN=dir([dirProc 'Project\TrackMask_' num2str(i) '\' '*.bat']);
+        system([dirProc 'Project\TrackMask_' num2str(i) '\' fileNN.name]);
 %         while ~exist([dirout 'Output\TrackMask_' num2str(i) '\blank.txt'])
 %            pause(300);
 %            fprintf('ne')
@@ -197,7 +200,7 @@ end
 %% Determine brightness threshold for each track
 GlobalOrLocal=0; % Check if a global brightness threshold is defined for the area (1), or a local one for each image (0).
 arrayHist=0:16:256*256;% For 16bit
-dirProcessed=[dirout 'Output\'];
+dirProcessed=[dirProc 'Output\'];
 localStep=3;%A range determining how many images are included into the analysis for local threshold (200 would indicate up to 100 images taken before or after the current one)
 peakPercentage=20;%what percentage of peak magnitude is taken as a breaking threshold (i.e. 10 would mean 10%)
 
@@ -235,17 +238,17 @@ for i=11:12
 end
 
 if GlobalOrLocal==1
-    dirV=[dirout 'Saved_Matlab_Data\'];
+    dirV=[dirProc 'Saved_Matlab_Data\'];
     if ~exist(dirV, 'dir'), mkdir(dirV); end
     save([dirV 'thresh_global.mat'],'Threshold','N');
 elseif GlobalOrLocal==0
     Threshold=denoiseThreshold(Threshold,tracks,trackTag);
-    dirV=[dirout 'Saved_Matlab_Data\'];
+    dirV=[dirProc 'Saved_Matlab_Data\'];
     if ~exist(dirV, 'dir'), mkdir(dirV); end
     save([dirV 'thresh_local_NewMethod.mat'],'Threshold','N');
 end
 % plot out graphs for determining threshold, and give some examples
-dirV=[dirout 'Quality_Control\Brightness_Threshold_New\'];
+dirV=[dirProc 'Quality_Control\Brightness_Threshold_New\'];
 if ~exist(dirV, 'dir'), mkdir(dirV); end
 plot_Thresh(Threshold,N,dirProcessed,dirV,D_Im,tracks_Im,trackTag,tracks,arrayHist,Glint,Glint_mask,meanIm,GlobalOrLocal);
 
@@ -264,16 +267,16 @@ plot_Thresh(Threshold,N,dirProcessed,dirV,D_Im,tracks_Im,trackTag,tracks,arrayHi
 for i=11:1:12%1:length(tracks)
     if trackTag(i).stable==1
         if i>=10
-            dirV=[dirout 'Lambdas_inbuilt_Threshold_0_pyramidScaled_V5\Track_' num2str(i) '\'];
+            dirV=[dirProc 'Lambdas_inbuilt_Threshold_0_pyramidScaled_V5\Track_' num2str(i) '\'];
         else
-            dirV=[dirout 'Lambdas_inbuilt_Threshold_0_pyramidScaled_V5\Track_0' num2str(i) '\'];
+            dirV=[dirProc 'Lambdas_inbuilt_Threshold_0_pyramidScaled_V5\Track_0' num2str(i) '\'];
         end
         if ~exist(dirV, 'dir'), mkdir(dirV); end
         
        
         
-        dirinIM=[dirout 'Output\Track_' num2str(i) '\'];
-        dirinIM_mask=[dirout 'Output\TrackMask_' num2str(i) '\'];
+        dirinIM=[dirProc 'Output\Track_' num2str(i) '\'];
+        dirinIM_mask=[dirProc 'Output\TrackMask_' num2str(i) '\'];
         filenamesIM=dir([dirinIM '*.tif']);
         
         offSetEnds=0;
@@ -309,34 +312,34 @@ end
 
 
 % Plot out randomly chosen examples from the track
-dirV=[dirout 'Quality_Control\LambdaIms_inbuilt\'];
+dirV=[dirProc 'Quality_Control\LambdaIms_inbuilt\'];
 if ~exist(dirV, 'dir'), mkdir(dirV); end
 
-dirV2=[dirout 'Plots\DistributionsAll\'];
+dirV2=[dirProc 'Plots\DistributionsAll\'];
 if ~exist(dirV2, 'dir'), mkdir(dirV2); end
 
-dirV2=[dirout 'Plots\DistributionsAll_WC_Glint\'];
+dirV2=[dirProc 'Plots\DistributionsAll_WC_Glint\'];
 if ~exist(dirV2, 'dir'), mkdir(dirV2); end
 
-dirV2=[dirout 'Plots\DistributionsAll_new\'];
+dirV2=[dirProc 'Plots\DistributionsAll_new\'];
 if ~exist(dirV2, 'dir'), mkdir(dirV2); end
 
-dirV2=[dirout 'Plots\Distributions_Inbuilt\'];
+dirV2=[dirProc 'Plots\Distributions_Inbuilt\'];
 if ~exist(dirV2, 'dir'), mkdir(dirV2); end
 
 
-dirV3=[dirout 'Plots\Summary\'];
+dirV3=[dirProc 'Plots\Summary\'];
 if ~exist(dirV3, 'dir'), mkdir(dirV3); end
 
-dirV4=[dirout 'Plots\Alongtrack_OppositeDirection\'];
+dirV4=[dirProc 'Plots\Alongtrack_OppositeDirection\'];
 if ~exist(dirV4, 'dir'), mkdir(dirV4); end
 
-dirV4=[dirout 'Plots\Alongtrack_test3\'];
+dirV4=[dirProc 'Plots\Alongtrack_test3\'];
 if ~exist(dirV4, 'dir'), mkdir(dirV4); end
 addpath('utilities')
 
 
-dirV2=[dirout 'Plots\DistributionsAll_new\'];
+dirV2=[dirProc 'Plots\DistributionsAll_new\'];
 if ~exist(dirV2, 'dir'), mkdir(dirV2); end
 %new - 0 
 %new2 - 10
@@ -346,12 +349,12 @@ for i=11:1:11%1:length(tracks)
     i
     if trackTag(i).stable==1
         if i>=10
-            dirL=[dirout 'Lambdas_inbuilt\Track_' num2str(i) '\'];
+            dirL=[dirProc 'Lambdas_inbuilt\Track_' num2str(i) '\'];
         else
-            dirL=[dirout 'Lambdas_inbuilt\Track_0' num2str(i) '\'];
+            dirL=[dirProc 'Lambdas_inbuilt\Track_0' num2str(i) '\'];
         end
         filenames=dir([dirL '*.mat']);
-        dirinIM=[dirout 'Output\Track_' num2str(i) '\'];
+        dirinIM=[dirProc 'Output\Track_' num2str(i) '\'];
         
         filenamesIM=dir([dirinIM '*.tif']);
         pozicija=find('_'==filenamesIM(1).name,2);
@@ -365,7 +368,7 @@ for i=11:1:11%1:length(tracks)
         strN=filenamesIM(1).name;
         PARAM.dx = str2num(strN(pozicija+1:pozicija+2))/100;
         
-        dirin_img = [dirout 'Output\Track_' num2str(i) '\'];
+        dirin_img = [dirProc 'Output\Track_' num2str(i) '\'];
         filenamesI=dir([dirin_img '*.tif']);
         
         %determineDists(dirV2,i,dirL,filenames,dxIm,tracks,A,Threshold);

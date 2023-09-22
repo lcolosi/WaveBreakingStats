21e3b7ba8e5bf4a4e5e0cc54106c261264174c78
Luke Colosi <lcolosi@ucsd.edu>
2023-09-22 04:20:21 +0000

    Begin documentation.

--- tools/vignette_removal/DefineTracksIm.m
+++ tools/vignette_removal/DefineTracksIm.m
@@ -1,10 +1,43 @@
 function tracks_Im = DefineTracksIm(D_Im,tracks)
 
+    %%%%
+    % tracks_Im = DefineTracksIm(D_Im,tracks)
+    %
+    % Function for 
+    %
+    %   Parameters
+    %   ----------
+    %   D_Im : Filenames of the non-georeferenced video images.
+    %   tracks : Structure containing the start and end time indicies of
+    %            each flight track.
+    % 
+    %   Returns
+    %   -------
+    %   track_Im : Structure with two fields: 
+    %               (1) stable: An identifier for whether the Track is 
+    %                           stable or unstable based on the inputted
+    %                           roll, pitch, and heading criteria. Here, we
+    %                           denote stability with the following
+    %                           convention:
+    %                                   0 -> Unstable
+    %                                   1 -> Stable    
+    %               (2) range: The start and end time indices of the stable
+    %                          flight track.  
+    %%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Determine indices of tracks, and organize them. Assume that the images 
 % are defined in form of : CoreView_TrackNumber.......
 
+% 
 Indices=zeros(length(D_Im(:,1)),1);
+
+% Loop through nongeoreferenced files
 for i=1:length(D_Im(:,1))
+
+    % 
     arrTemp=D_Im(i,1).name;
     k=find('_'==arrTemp,3,'first');
     Indices(i)=str2num(arrTemp(k(2)+1:k(3)-1));

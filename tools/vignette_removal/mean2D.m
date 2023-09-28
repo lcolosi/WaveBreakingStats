24fa478bc1f17d9be45b96a51739a5d9796df49b
Luke Colosi <lcolosi@ucsd.edu>
2023-09-28 21:13:34 +0000

    Clean up mean2D

--- tools/vignette_removal/mean2D.m
+++ tools/vignette_removal/mean2D.m
@@ -1,8 +1,38 @@
 function meanIm_2=mean2D(meanIm,winSize)
 
-% A utility 2d moving average function.
+    %%%%
+    % meanIm_2=mean2D(meanIm,winSize)
+    %
+    % Function for computing a 2D moving average of an array using a square
+    % kernel of side length winSize. 
+    % 
+    %
+    %   Parameters
+    %   ----------
+    %   MeanIm   : NxM array; cannot contain NaNs.   
+    %   winSize  : Side length of the square kernel.   
+    % 
+    %   Returns
+    %   -------
+    %   meanIm_2 : 2D averaged array of the same size as the input array.
+    %  
+    % 
+    %%%%
 
-tempIm=ones(size(meanIm));
-tempIm=conv2(tempIm,ones(winSize,winSize)/winSize^2,'same');
+    % Initialize kernel
+    k = ones(winSize,winSize)/winSize^2;
 
-meanIm_2=conv2(meanIm,ones(winSize,winSize)/winSize^2,'same')./tempIm;
+    % Convolve the mean image with the kernel (the 'same' argument returns
+    % the central part of the convolution, which is the same size as tempIm)
+    meanIm_2=conv2(meanIm,k,'same'); 
+
+end
+
+%% Developmental code 
+% % Initialize a ones array 
+% tempIm=ones(size(meanIm));
+% 
+% % Convolve the ones array with the kernel (same returns the central 
+% % part of the convolution, which is the same size as tempIm)
+% tempIm=conv2(tempIm,k,'same');
+% meanIm_2=conv2(meanIm,k,'same')./tempIm;
